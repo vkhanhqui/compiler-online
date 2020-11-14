570c9f503f0192b2596cde0778db0a4e6b61cafe
Khanh Qui <vkhanhqui@gmail.com>
2020-11-15 00:02:06 +0700

    add function to read file .c

--- src/main/java/com/fundamental/compiler/FundamentalCompilerApplication.java
+++ src/main/java/com/fundamental/compiler/FundamentalCompilerApplication.java
@@ -6,11 +6,34 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 @SpringBootApplication
 public class FundamentalCompilerApplication {
 
+    public static void results() {
+        try {
+            String location = "G:\\tailieuREstore\\java-Spring-framework\\implementation\\fundamental-compiler\\result-dir";
+            File dir = new File(location);
+            Runtime runtime = Runtime.getRuntime();
+            runtime.exec("gcc de1bai3.c", null, dir);
+            Process processRS = runtime.exec(location+"\\a.exe", null, dir);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(processRS.getInputStream()));
+            String rs = "";
+            while ((rs = reader.readLine()) != null) {
+                System.out.println(rs);
+            }
+        } catch (IOException io) {
+            System.out.println(io);
+        }
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(FundamentalCompilerApplication.class, args);
+        results();
     }
 
     @Bean
--- src/main/java/com/fundamental/compiler/controller/FileUploadController.java
+++ src/main/java/com/fundamental/compiler/controller/FileUploadController.java
@@ -6,31 +6,31 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.ui.Model;
 
 import java.util.stream.Collectors;
 
 @Controller
+@RequestMapping("upload")
 public class FileUploadController {
     @Autowired
     StorageService storageService;
 
-    @GetMapping("upload")
+    @GetMapping
     public String uploadFile(Model model) {
 
         model.addAttribute("files", storageService.loadAll().map(
                 path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
                         "serveFile", path.getFileName().toString()).build().toUri().toString())
                 .collect(Collectors.toList()));
-
         return "uploadForm";
     }
 
-    @GetMapping("/files/{filename:.+}")
+    @PostMapping("files/{filename:.+}")
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
@@ -39,7 +39,7 @@ public class FileUploadController {
                 "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 
-    @PostMapping("upload")
+    @PostMapping
     public String handleFileUpload(@RequestParam("file") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
 
